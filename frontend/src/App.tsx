64662d1b792d8be6a8c4f66d2f7b7558fcbdb658
Grokr-Purvi <purvi.modi@datagrokr.com>
2022-05-19 19:02:35 +0530

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { LandingPage } from "./Components/LandingPage";
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
+}
+
+export default App;
